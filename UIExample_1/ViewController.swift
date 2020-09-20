1d33e130fa8953a900dc47933db9b6fa3d468437
Alexandre Hauffe <alex_hotmail.com>
2020-09-19 21:35:19 -0300

    Some UI things!

--- UIExample_1/ViewController.swift
+++ UIExample_1/ViewController.swift
@@ -8,12 +8,39 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    @IBOutlet weak var activityIndicator: UIActivityIndicatorView!
+    @IBOutlet weak var progressView: UIProgressView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
-
-
+    
+    @IBAction func sliderDidChange(_ sender: UISlider) {
+        progressView.progress = sender.value
+        if progressView.progress >= 1.0{
+            activityIndicator.stopAnimating()
+        }else {
+            activityIndicator.startAnimating()
+        }
+    }
+      
+    @IBAction func didPressButton(_ sender: Any) {
+                
+        //shere data
+        /*
+        let activityView =
+            UIActivityViewController(activityItems: ["Data From this app"], applicationActivities: nil)
+            present(activityView, animated: true, completion: nil)
+        */
+        //Alert!
+        /*let alertController = UIAlertController(title: "Alert Title", message: "Alert Message", preferredStyle: .alert)
+        let alertAction = UIAlertAction(title: "OK", style: .cancel) { (action) in
+            print("All done!")
+        }
+        alertController.addAction(alertAction)
+        present(alertController, animated: true, completion: nil)*/
+    }
 }
 
